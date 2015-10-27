cf25dcdad6a2bb823554f177ea39472674f4495a
jc222fi <jc222fi@student.lnu.se>
2015-10-27 14:25:02 +0100

    progress

--- view/GameView.php
+++ view/GameView.php
@@ -11,20 +11,21 @@ class GameView{
     public function generateGameBoard(){
         $ret = "<form id='gameBoard' method='post' >";
         $ret2 = "";
-        $inputBox = $this->getInputBox();
-        for($i = 1; $i <= 3; $i++){
-            $ret2 .= "<div id='row". $i ."'>$inputBox</div>";
-        }
 
+        for($i = 0; $i <=8; $i++){
+            $ret2 .= "<input type='text' name='box". $i ."' value=''/>";
+            if($i==2 || $i==5 || $i==8){
+                $ret2 .="<br>";
+            }
+        }
 		$ret3= "    <input type='submit' name='submit' value='Submit'/>
                 </form>";
         return $ret . $ret2 . $ret3;
     }
-    private function getInputBox(){
-        $ret= "";
-        for($i=1;$i<=3;$i++){
-            $ret .= "<input type='text' name='column". $i ."' value=''/>";
+    public function submitForm(){
+        if(isset($_POST["submit"])){
+            return true;
         }
-        return $ret;
+        return false;
     }
 }
